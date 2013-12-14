66c9171f037ff38ccad5ad671863d81aa973823f
chrmhoffmann <chrmhoffmann@gmail.com>
2013-12-14 13:22:27 +0100

    acclaim: use toolbox to mount to fix mac
    Change-Id: I34c21e8768ebd10440b6f79273f89136f3d7b178

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -4,13 +4,10 @@ PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
 ORIG_NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
 NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 
-busybox mount -o remount,rw /system
-
-if busybox [ ! -f "$NVS_BIN" ]; then
-    cp ${ORIG_NVS_BIN} ${NVS_BIN}
+if bbx [ ! -f "$NVS_BIN" ]; then
+    toolbox mount -o remount,rw /system
+    toolbox cp ${ORIG_NVS_BIN} ${NVS_BIN}
+    MAC=`cat /rom/devconf/MACAddress | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3:\4:\5:/'`
+    calibrator set nvs_mac $NVS_BIN $MAC
+    toolbox mount -o remount,ro /system
 fi
-
-MAC=`cat /rom/devconf/MACAddress | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3:\4:\5:/'`
-calibrator set nvs_mac $NVS_BIN $MAC
-
-busybox mount -o remount,ro /system
