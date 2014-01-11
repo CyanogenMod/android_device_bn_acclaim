1f673f1c16140402acc7b2ffd3ebdb30cb277380
chrmhoffmann <chrmhoffmann@gmail.com>
2014-01-11 13:07:18 +0100

    bootanim: fix pvr crash by using 480 version thx to hashcode.
    Change-Id: I6e2c505ccc5d843b44e80d6be7f0e3c1bfa4f61b

--- cm.mk
+++ cm.mk
@@ -16,7 +16,7 @@
 
 $(call inherit-product, device/bn/acclaim/full_acclaim.mk)
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
-TARGET_SCREEN_WIDTH := 600
+TARGET_SCREEN_WIDTH := 480
 TARGET_SCREEN_HEIGHT := 1024
 
 DEVICE_PACKAGE_OVERLAYS += device/bn/acclaim/overlay/cm
