8c8bc566cded9965601e51a7d9ffd9dae1ae96b1
chrmhoffmann <chrmhoffmann@gmail.com>
2015-02-07 11:21:22 +0100

    acclaim: don't mount/remount when clearing bootdata, we need to set reboot command to BCB later, too.
    Change-Id: I63f68678d5c1bb2db745e868c382901c105f8f49

--- prebuilt/bin/clearbootdata.sh
+++ prebuilt/bin/clearbootdata.sh
@@ -1,12 +1,8 @@
 #!/system/bin/sh
 
-mount /bootdata
-
 # Zero out the boot counter
 dd if=/dev/zero of=/bootdata/BootCnt bs=1 count=4
 
 # Reset the bootloader control block (bcb) file
 dd if=/dev/zero of=/bootdata/BCB bs=1 count=1088
-
-umount /bootdata
 
