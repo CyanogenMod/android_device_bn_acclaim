55a7634df38fb72f5b7b9c5db319ab680cee41bb
Steven Luo <steven@steven676.net>
2015-11-17 21:09:24 -0600

    Acclaim: Set wifi MAC address
    Use a symlink from /system/etc/firmware to /data/misc/wifi
    to avoid having to mount /system to set MAC address.
    
    The wifi MAC address assigned by the manufacturer is stored in a file in
    /rom/devconf, but the wl12xx driver expects the MAC address to either be
    burned into the board or stored in the wl1271-nvs.bin firmware file.
    Add a script which reads the MAC address and stores it into the
    wl1271-nvs.bin file, and run it on startup.
    
    Borrowed from steven@steven676.net encore code.
    
    Change-Id: I70955c28a75b114c8faa1d7b13e5fab2d7e8db47

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -1,14 +1,53 @@
 #!/system/bin/sh
+# store-mac-addr -- reads configured wifi MAC address and writes it into nvs
+# file for use by the wl12xx driver
 
-ORIG_NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
-NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
+ROM_NVS=/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
+ORIG_NVS=/data/misc/wifi/wl1271-nvs.bin.orig
+NEW_NVS=/data/misc/wifi/wl1271-nvs.bin
+MACADDR_FILE=/rom/devconf/MACAddress
+MACADDR_COPY=/data/misc/wifi/MACAddress
 
-if [ ! -f "$NVS_BIN" ]; then
-    mount -o remount,rw /system
-    cp ${ORIG_NVS_BIN} ${NVS_BIN}
-    MAC=`cat /rom/devconf/MACAddress | sed 's/\(..\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3:\4:\5:/'`
-    calibrator set nvs_mac $NVS_BIN $MAC
-    mount -o remount,ro /system
-fi
+PATH=/vendor/bin:/system/bin:/system/xbin
+umask 0022
+
+# Don't bother updating the nvs file if the one shipped in the ROM hasn't
+# changed since the last boot and the MAC address of the device hasn't changed
+cmp "$ROM_NVS" "$ORIG_NVS" > /dev/null 2>&1 && \
+    cmp "$MACADDR_FILE" "$MACADDR_COPY" > /dev/null 2>&1 && \
+    insmod /system/lib/modules/wl12xx_sdio.ko && \
+    exit 0
+
+# Get the MAC address
+macaddr=$(cat "$MACADDR_FILE")
+[ $macaddr ] || exit 1
+
+# The MAC address is stored in the nvs file in two pieces: the four
+# least-significant bytes in little-endian order starting at byte offset 3
+# (indexed to 0), and the two most-significant bytes in little-endian order
+# starting at byte offset 10.
+#
+# We're using printf to write these bytes to the file, so parse the MAC
+# address to produce the escape sequences we'll use as arguments to printf.
+lowbytes=$(echo "$macaddr" | sed -e 's#^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)$#\\x\6\\x\5\\x\4\\x\3#')
+highbytes=$(echo "$macaddr" | sed -e 's#^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)$#\\x\2\\x\1#')
+
+# Create the new nvs file by copying over the ROM's copy byte by byte,
+# replacing only the pieces containing the MAC address
+dd if="$ROM_NVS" of="$NEW_NVS" bs=1 count=3
+printf "$lowbytes" >> "$NEW_NVS"
+dd if="$ROM_NVS" of="$NEW_NVS" bs=1 skip=7 seek=7 count=3
+printf "$highbytes" >> "$NEW_NVS"
+dd if="$ROM_NVS" of="$NEW_NVS" bs=1 skip=12 seek=12
+
+# Store the unmodified nvs file for reference
+cp "$ROM_NVS" "$ORIG_NVS"
+
+# Also store the MAC address referenced in the NVS, so that we can detect the
+# case where this installation is cloned/moved to another device and update
+# the NVS file accordingly
+cp "$MACADDR_FILE" "$MACADDR_COPY"
 
 insmod /system/lib/modules/wl12xx_sdio.ko
+
+exit 0
